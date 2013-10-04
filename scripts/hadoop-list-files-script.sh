a4a6baacd1adadf4b1b0a855dbe539537742cfdc
Albert Chu <chu11@llnl.gov>
2013-10-04 16:05:44 -0700

    Add new convenience script hadoop-list-files-script.sh

---
+++ scripts/hadoop-list-files-script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This script just lists the files in your HDFS home dir.
+#
+# Convenient for just looking to see what's in there, perhaps after a job
+
+cd ${HADOOP_BUILD_HOME}
+
+command="bin/hadoop fs -ls"
+echo "Running $command" >&2
+$command
+
+exit 0
