955e1f581e227900a21b649a94390348891f16ef
Albert Chu <chu11@llnl.gov>
2013-12-12 10:31:58 -0800

    Add additional detail to hadoop-put-into-hdfs-over-lustre.sh

--- scripts/hadoop-put-into-hdfs-over-lustre.sh
+++ scripts/hadoop-put-into-hdfs-over-lustre.sh
@@ -2,6 +2,11 @@
 
 # This is a basic script to move from Lustre into HDFS over Lustre.
  
+cd ${HADOOP_HOME}
+
 # hadoop fs -copyFromLocal <lustre-dir> <hdfs-dir>
+command="hadoop fs -copyFromLocal FOO BAR"
+echo "Running $command" >&2
+$command
 
 exit 0
