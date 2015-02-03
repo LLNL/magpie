83346861362eeecbee6701393f3b13de19f3c334
Albert Chu <chu11@llnl.gov>
2015-02-02 16:07:02 -0800

    Check if conf dir exists before looking for files

--- scripts/pre-job-run-scripts/magpie-output-config-files-script.sh
+++ scripts/pre-job-run-scripts/magpie-output-config-files-script.sh
@@ -13,82 +13,94 @@ then
 
     # Cat conf files for documentation
 
-    if [ -f "${HADOOP_CONF_DIR}/hadoop-env.sh" ] 
+    if [ "${HADOOP_CONF_DIR}X" != "X" ]
     then
-	echo "**** hadoop-env.sh ****"
-	cat ${HADOOP_CONF_DIR}/hadoop-env.sh
+	if [ -f "${HADOOP_CONF_DIR}/hadoop-env.sh" ] 
+	then
+	    echo "**** hadoop-env.sh ****"
+	    cat ${HADOOP_CONF_DIR}/hadoop-env.sh
+	fi
+	
+	if [ -f "${HADOOP_CONF_DIR}/mapred-env.sh" ] 
+	then
+	    echo "**** mapred-env.sh ****"
+	    cat ${HADOOP_CONF_DIR}/mapred-env.sh
+	fi
+
+	if [ -f "${HADOOP_CONF_DIR}/yarn-env.sh" ] 
+	then
+	    echo "**** yarn-env.sh ****"
+	    cat ${HADOOP_CONF_DIR}/yarn-env.sh
+	fi
+
+	if [ -f "${HADOOP_CONF_DIR}/mapred-site.xml" ]
+	then
+	    echo "**** mapred-site.xml ****"
+	    cat ${HADOOP_CONF_DIR}/mapred-site.xml
+	fi
+
+	if [ -f "${HADOOP_CONF_DIR}/core-site.xml" ]
+	then
+	    echo "**** core-site.xml ****"
+	    cat ${HADOOP_CONF_DIR}/core-site.xml
+	fi
+
+	if [ -f "${HADOOP_CONF_DIR}/hdfs-site.xml" ] 
+	then
+	    echo "**** hdfs-site.xml ****"
+	    cat ${HADOOP_CONF_DIR}/hdfs-site.xml
+	fi
+
+	if [ -f "${HADOOP_CONF_DIR}/yarn-site.xml" ] 
+	then
+	    echo "**** yarn-site.xml ****"
+	    cat ${HADOOP_CONF_DIR}/yarn-site.xml
+	fi
     fi
 
-    if [ -f "${HADOOP_CONF_DIR}/mapred-env.sh" ] 
+    if [ "${HBASE_CONF_DIR}X" != "X" ]
     then
-	echo "**** mapred-env.sh ****"
-	cat ${HADOOP_CONF_DIR}/mapred-env.sh
+	if [ -f "${HBASE_CONF_DIR}/hbase-env.sh" ] 
+	then
+	    echo "**** hbase-env.sh ****"
+	    cat ${HBASE_CONF_DIR}/hbase-env.sh
+	fi
+
+	if [ -f "${HBASE_CONF_DIR}/hbase-site.xml" ]
+	then
+	    echo "**** hbase-site.xml ****"
+	    cat ${HBASE_CONF_DIR}/hbase-site.xml
+	fi
     fi
 
-    if [ -f "${HADOOP_CONF_DIR}/yarn-env.sh" ] 
+    if [ "${SPARK_CONF_DIR}X" != "X" ]
     then
-	echo "**** yarn-env.sh ****"
-	cat ${HADOOP_CONF_DIR}/yarn-env.sh
+	if [ -f "${SPARK_CONF_DIR}/spark-env.sh" ] 
+	then
+	    echo "**** spark-env.sh ****"
+	    cat ${SPARK_CONF_DIR}/spark-env.sh
+	fi
+	
+	if [ -f "${SPARK_CONF_DIR}/spark-defaults.conf" ] 
+	then
+	    echo "**** spark-defaults.conf ****"
+	    cat ${SPARK_CONF_DIR}/spark-defaults.conf
+	fi
     fi
 
-    if [ -f "${HADOOP_CONF_DIR}/mapred-site.xml" ]
+    if [ "${STORM_CONF_DIR}X" != "X" ]
     then
-	echo "**** mapred-site.xml ****"
-	cat ${HADOOP_CONF_DIR}/mapred-site.xml
-    fi
-
-    if [ -f "${HADOOP_CONF_DIR}/core-site.xml" ]
-    then
-	echo "**** core-site.xml ****"
-	cat ${HADOOP_CONF_DIR}/core-site.xml
-    fi
-
-    if [ -f "${HADOOP_CONF_DIR}/hdfs-site.xml" ] 
-    then
-	echo "**** hdfs-site.xml ****"
-	cat ${HADOOP_CONF_DIR}/hdfs-site.xml
-    fi
-
-    if [ -f "${HADOOP_CONF_DIR}/yarn-site.xml" ] 
-    then
-	echo "**** yarn-site.xml ****"
-	cat ${HADOOP_CONF_DIR}/yarn-site.xml
-    fi
-
-    if [ -f "${HBASE_CONF_DIR}/hbase-env.sh" ] 
-    then
-	echo "**** hbase-env.sh ****"
-	cat ${HBASE_CONF_DIR}/hbase-env.sh
-    fi
-
-    if [ -f "${HBASE_CONF_DIR}/hbase-site.xml" ]
-    then
-	echo "**** hbase-site.xml ****"
-	cat ${HBASE_CONF_DIR}/hbase-site.xml
-    fi
-
-    if [ -f "${SPARK_CONF_DIR}/spark-env.sh" ] 
-    then
-	echo "**** spark-env.sh ****"
-	cat ${SPARK_CONF_DIR}/spark-env.sh
-    fi
-
-    if [ -f "${SPARK_CONF_DIR}/spark-defaults.conf" ] 
-    then
-	echo "**** spark-defaults.conf ****"
-	cat ${SPARK_CONF_DIR}/spark-defaults.conf
-    fi
-
-    if [ -f "${STORM_CONF_DIR}/storm-env.sh" ] 
-    then
-	echo "**** storm-env.sh ****"
-	cat ${STORM_CONF_DIR}/storm-env.sh
-    fi
-
-    if [ -f "${STORM_CONF_DIR}/storm.yaml" ] 
-    then
-	echo "**** storm.yaml ****"
-	cat ${STORM_CONF_DIR}/storm.yaml
+	if [ -f "${STORM_CONF_DIR}/storm-env.sh" ] 
+	then
+	    echo "**** storm-env.sh ****"
+	    cat ${STORM_CONF_DIR}/storm-env.sh
+	fi
+	
+	if [ -f "${STORM_CONF_DIR}/storm.yaml" ] 
+	then
+	    echo "**** storm.yaml ****"
+	    cat ${STORM_CONF_DIR}/storm.yaml
+	fi
     fi
 fi
 
@@ -96,16 +108,19 @@ if [ "${MAGPIE_CLUSTER_NODERANK}" == "1" ]
 then
     # Cat conf files for documentation
 
-    if [ -f "${ZOOKEEPER_CONF_DIR}/zoo.cfg" ] 
+    if [ "${ZOOKEEPER_CONF_DIR}X" != "X" ]
     then
-	echo "**** zoo.cfg ****"
-	cat ${ZOOKEEPER_CONF_DIR}/zoo.cfg
-    fi
-
-    if [ -f "${ZOOKEEPER_CONF_DIR}/zookeeper-env.sh" ] 
-    then
-	echo "**** zookeeper-env.sh ****"
-	cat ${ZOOKEEPER_CONF_DIR}/zookeeper-env.sh
+	if [ -f "${ZOOKEEPER_CONF_DIR}/zoo.cfg" ] 
+	then
+	    echo "**** zoo.cfg ****"
+	    cat ${ZOOKEEPER_CONF_DIR}/zoo.cfg
+	fi
+	
+	if [ -f "${ZOOKEEPER_CONF_DIR}/zookeeper-env.sh" ] 
+	then
+	    echo "**** zookeeper-env.sh ****"
+	    cat ${ZOOKEEPER_CONF_DIR}/zookeeper-env.sh
+	fi
     fi
 fi
 
