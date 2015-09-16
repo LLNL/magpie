61524f61a3607a0d83a6da8eff2f129f2d0d7546
Albert Chu <chu11@llnl.gov>
2015-09-16 09:58:16 -0700

    Fix path bug in test scripts

--- testsuite/mytestpig.sh
+++ testsuite/mytestpig.sh
@@ -2,7 +2,7 @@
 
 cd $HADOOP_HOME
 bin/hadoop fs -mkdir -p hdfs:///user/achu
-bin/hadoop fs -copyFromLocal ${MAGPIE_SCRIPTS_HOME}/submission-scripts/tests/mypigdata hdfs:///user/achu/mypigdata
+bin/hadoop fs -copyFromLocal ${MAGPIE_SCRIPTS_HOME}/testsuite/mypigdata hdfs:///user/achu/mypigdata
 
 cd $PIG_HOME
-bin/pig -stop_on_failure -l ${MAGPIE_SCRIPTS_HOME}/submission-scripts/tests/mytestpig.log ${MAGPIE_SCRIPTS_HOME}/submission-scripts/tests/mytest.pig
+bin/pig -stop_on_failure -l ${MAGPIE_SCRIPTS_HOME}/testsuite/mytestpig.log ${MAGPIE_SCRIPTS_HOME}/testsuite/mytest.pig
