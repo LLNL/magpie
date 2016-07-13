7c8a36e08c88daeba161b43712b90dc0feb0370e
Albert Chu <chu11@llnl.gov>
2016-07-12 20:21:28 -0700

    For consistency to many big data projects, create 'start-all' and 'stop-all' equivalent scripts in bin/

---
+++ bin/magpie-kafka-start-all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#############################################################################
+#  Copyright (C) 2013-2015 Lawrence Livermore National Security, LLC.
+#  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+#  Written by Albert Chu <chu11@llnl.gov>
+#  LLNL-CODE-644248
+#  
+#  This file is part of Magpie, scripts for running Hadoop on
+#  traditional HPC systems.  For details, see https://github.com/llnl/magpie.
+#  
+#  Magpie is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  Magpie is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with Magpie.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+
+# This script starts Kafka across all nodes for the user
+
+if [ "${MAGPIE_SCRIPTS_HOME}X" != "X" ]
+then
+    export MAGPIE_SCRIPTS_HOME=$(cd "`dirname "$0"`"/..; pwd)
+fi
+
+${MAGPIE_SCRIPTS_HOME}/bin/magpie-kafka-daemons.sh start
