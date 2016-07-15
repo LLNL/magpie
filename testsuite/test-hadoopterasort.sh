d2f7899cfa3de13411037e7903f05fb55ee4c6ed
Albert Chu <chu11@llnl.gov>
2016-07-15 08:12:23 -0700

    Fix deprecation, using proper config keywords in terasort run

--- testsuite/test-hadoopterasort.sh
+++ testsuite/test-hadoopterasort.sh
@@ -5,8 +5,16 @@ terasortexamples="share/hadoop/mapreduce/hadoop-mapreduce-examples-$HADOOP_VERSI
 # So output paths aren't the same
 uniquestring=`date +"%Y%m%d%N"`
 
-# reduce.tasks = HADOOP_SLAVE_COUNT to make it easy & simple
-command="bin/hadoop jar ${terasortexamples} terasort -Dmapred.reduce.tasks=${HADOOP_SLAVE_COUNT} test-teragen test-terasort-${uniquestring}"
+# reduce tasks = HADOOP_SLAVE_COUNT to make it easy & simple
+if [ "${HADOOP_SETUP_TYPE}" == "MR1" ]
+then
+    mapreducereducetasks="-Dmapred.reduce.tasks=${HADOOP_SLAVE_COUNT}"
+elif [ "${HADOOP_SETUP_TYPE}" == "MR2" ]
+then
+    mapreducereducetasks="-Dmapreduce.job.reduces=${HADOOP_SLAVE_COUNT}"
+fi
+
+command="bin/hadoop jar ${terasortexamples} terasort ${mapreducereducetasks} test-teragen test-terasort-${uniquestring}"
 echo "Running $command" >&2
 $command
 
