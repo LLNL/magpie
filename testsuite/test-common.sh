550d6ade5b13ec92b2142456df3fb4d47d4e7e03
Albert Chu <chu11@llnl.gov>
2016-07-15 18:03:00 -0700

    Make test dependencies of projects based on 'base' 0 project to limit permutation of tests

--- testsuite/test-common.sh
+++ testsuite/test-common.sh
@@ -16,28 +16,28 @@ hadoop_test_groups_decommission="hadoop2Xjava16versionsdecommission hadoop2Xjava
 hadoop_all_versions="${hadoop2Xjava16versions} ${hadoop2Xjava17versions}"
 
 mahouthadoop27java17versions="0.11.0 0.11.1 0.11.2 0.12.0 0.12.1"
-mahouthadoop27java17versions_hadoopversion="2.7.2"
+mahouthadoop27java17versions_hadoopversion="2.7.0"
 mahouthadoop27java17versions_javaversion=${java17}
 
 mahout_test_groups="mahouthadoop27java17versions"
 mahout_all_versions="${mahouthadoop27java17versions}"
 
 pighadoop26java16versions="0.13.0 0.14.0"
-pighadoop26java16versions_hadoopversion="2.6.4"
+pighadoop26java16versions_hadoopversion="2.6.0"
 pighadoop26java16versions_javaversion=${java16}
 pighadoop27java17versions="0.15.0"
-pighadoop27java17versions_hadoopversion="2.7.2"
+pighadoop27java17versions_hadoopversion="2.7.0"
 pighadoop27java17versions_javaversion=${java17}
 
 pig_test_groups="pighadoop26java16versions pighadoop27java17versions"
 pig_all_versions="${pighadoop26java16versions} ${pighadoop27java17versions}"
 
 hbasehadoop26zookeeper34java16versions="0.98.3-hadoop2 0.98.9-hadoop2"
-hbasehadoop26zookeeper34java16versions_hadoopversion="2.6.4"
+hbasehadoop26zookeeper34java16versions_hadoopversion="2.6.0"
 hbasehadoop26zookeeper34java16versions_javaversion=${java16}
 hbasehadoop26zookeeper34java16versions_zookeeperversion="3.4.6"
 hbasehadoop27zookeeper34java17versions="0.99.0 0.99.1 0.99.2 1.0.0 1.0.1 1.0.1.1 1.0.2 1.1.0 1.1.0.1 1.1.1 1.1.2 1.1.3 1.1.4 1.2.0 1.2.1"
-hbasehadoop27zookeeper34java17versions_hadoopversion="2.7.2"
+hbasehadoop27zookeeper34java17versions_hadoopversion="2.7.0"
 hbasehadoop27zookeeper34java17versions_zookeeperversion="3.4.8"
 hbasehadoop27zookeeper34java17versions_javaversion=${java17}
 
@@ -45,8 +45,8 @@ hbase_test_groups="hbasehadoop26zookeeper34java16versions hbasehadoop27zookeeper
 hbase_all_versions="${hbasehadoop26zookeeper34java16versions} ${hbasehadoop27zookeeper34java17versions}"
 
 phoenixhbase11hadoop27zookeeper34java17versions="4.5.1-HBase-1.1 4.5.2-HBase-1.1 4.6.0-HBase-1.1 4.7.0-HBase-1.1"
-phoenixhbase11hadoop27zookeeper34java17versions_hbaseversion="1.1.4"
-phoenixhbase11hadoop27zookeeper34java17versions_hadoopversion="2.7.2"
+phoenixhbase11hadoop27zookeeper34java17versions_hbaseversion="1.1.0"
+phoenixhbase11hadoop27zookeeper34java17versions_hadoopversion="2.7.0"
 phoenixhbase11hadoop27zookeeper34java17versions_zookeeperversion="3.4.8"
 phoenixhbase11hadoop27zookeeper34java17versions_javaversion=${java17}
 
@@ -57,10 +57,10 @@ spark0Xjava16hadoop2versions="0.9.1-bin-hadoop2 0.9.2-bin-hadoop2"
 spark0Xjava16hadoop2versions_hadoopversion="2.2.0"
 spark0Xjava16hadoop2versions_javaversion=${java16}
 spark1Xjava16hadoop24versions="1.2.0-bin-hadoop2.4 1.2.1-bin-hadoop2.4 1.2.2-bin-hadoop2.4 1.3.0-bin-hadoop2.4 1.3.1-bin-hadoop2.4"
-spark1Xjava16hadoop24versions_hadoopversion="2.4.1"
+spark1Xjava16hadoop24versions_hadoopversion="2.4.0"
 spark1Xjava16hadoop24versions_javaversion=${java16}
 spark1Xjava17hadoop26versions="1.4.0-bin-hadoop2.6 1.4.1-bin-hadoop2.6 1.5.0-bin-hadoop2.6 1.5.1-bin-hadoop2.6 1.5.2-bin-hadoop2.6 1.6.0-bin-hadoop2.6 1.6.1-bin-hadoop2.6 1.6.2-bin-hadoop2.6"
-spark1Xjava17hadoop26versions_hadoopversion="2.6.4"
+spark1Xjava17hadoop26versions_hadoopversion="2.6.0"
 spark1Xjava17hadoop26versions_javaversion=${java17}
 
 spark_test_groups="spark0Xjava16hadoop2versions spark1Xjava16hadoop24versions spark1Xjava17hadoop26versions"
