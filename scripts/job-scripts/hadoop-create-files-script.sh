7f307c574c0296461100f6be1593d5a4754ff626
Albert Chu <chu11@llnl.gov>
2016-07-20 15:20:19 -0700

    Remove dead code in scripts/job-scripts/hadoop-create-files-script.sh

--- scripts/job-scripts/hadoop-create-files-script.sh
+++ scripts/job-scripts/hadoop-create-files-script.sh
@@ -8,11 +8,9 @@ cd ${HADOOP_HOME}
 if [ "${HADOOP_SETUP_TYPE}" == "MR1" ]
 then
     terasortexamples="hadoop-examples-$HADOOP_VERSION.jar"
-    rmoption="-rmr"
 elif [ "${HADOOP_SETUP_TYPE}" == "MR2" ]
 then
     terasortexamples="share/hadoop/mapreduce/hadoop-mapreduce-examples-$HADOOP_VERSION.jar"
-    rmoption="-rm -r"
 fi
 
 command="bin/hadoop jar ${terasortexamples} teragen 50000000 teragen-1"
