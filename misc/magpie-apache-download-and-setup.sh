5f7b21051a09062fabf5870af5e7f872280df61b
Albert Chu <chu11@llnl.gov>
2016-07-26 07:55:44 -0700

    Update download script to download hbase 1.2.2

--- misc/magpie-apache-download-and-setup.sh
+++ misc/magpie-apache-download-and-setup.sh
@@ -59,7 +59,7 @@ PRESET_LAUNCH_SCRIPT_CONFIGS="N"
 # have patches for every version.
 
 HADOOP_PACKAGE="hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz"
-HBASE_PACKAGE="hbase/1.2.1/hbase-1.2.1-bin.tar.gz"
+HBASE_PACKAGE="hbase/1.2.2/hbase-1.2.2-bin.tar.gz"
 PIG_PACKAGE="pig/pig-0.16.0/pig-0.16.0.tar.gz"
 MAHOUT_PACKAGE="mahout/0.12.2/apache-mahout-distribution-0.12.1.tar.gz"
 ZOOKEEPER_PACKAGE="zookeeper/zookeeper-3.4.8/zookeeper-3.4.8.tar.gz"
