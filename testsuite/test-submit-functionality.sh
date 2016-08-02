2dab11d1aa557f6da8474ee318378776936b187a
Albert Chu <chu11@llnl.gov>
2016-08-01 18:04:50 -0700

    Update testsuite functionality tests for Magpie 2.0 changes

--- testsuite/test-submit-functionality.sh
+++ testsuite/test-submit-functionality.sh
@@ -10,6 +10,7 @@ __SubmitFunctionalityTests_LegacySubmissionType() {
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-run-phoenixperformanceeval-functionality-legacysubmissiontype
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-legacysubmissiontype
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-legacysubmissiontype
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-legacysubmissiontype
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-legacysubmissiontype
     BasicJobSubmit magpie.${submissiontype}-storm-run-stormwordcount-functionality-legacysubmissiontype
     BasicJobSubmit magpie.${submissiontype}-spark-with-zeppelin-run-checkzeppelinup-functionality-legacysubmissiontype
@@ -30,6 +31,8 @@ __SubmitFunctionalityTests_BadJobNames () {
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-job-name-dollarsign
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-job-name-whitespace
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-job-name-dollarsign
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-job-name-whitespace
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-job-name-dollarsign
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-job-name-whitespace
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-job-name-dollarsign
     BasicJobSubmit magpie.${submissiontype}-storm-run-stormwordcount-functionality-job-name-whitespace
@@ -66,18 +69,23 @@ __SubmitFunctionalityTests_AltJobTimes () {
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-run-phoenixperformanceeval-functionality-altjobtime-sbatchsrun-days-hours-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-altjobtime-sbatchsrun-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-minutes-seconds
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-altjobtime-sbatchsrun-hours-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-hours-minutes-seconds
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-hours-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-hours-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-altjobtime-sbatchsrun-days-hours
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-days-hours
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-days-hours
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-days-hours
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-altjobtime-sbatchsrun-days-hours-minutes
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-days-hours-minutes
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-days-hours-minutes
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-days-hours-minutes
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-altjobtime-sbatchsrun-days-hours-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-days-hours-minutes-seconds
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-days-hours-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-altjobtime-sbatchsrun-days-hours-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-storm-run-stormwordcount-functionality-altjobtime-sbatchsrun-minutes-seconds
     BasicJobSubmit magpie.${submissiontype}-storm-run-stormwordcount-functionality-altjobtime-sbatchsrun-hours-minutes-seconds
@@ -99,6 +107,7 @@ __SubmitFunctionalityTests_AltConfFilesDir () {
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-run-phoenixperformanceeval-functionality-altconffilesdir
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-altconffilesdir
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-altconffilesdir
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-altconffilesdir
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-altconffilesdir
     BasicJobSubmit magpie.${submissiontype}-storm-run-stormwordcount-functionality-altconffilesdir
     BasicJobSubmit magpie.${submissiontype}-spark-with-zeppelin-run-checkzeppelinup-functionality-altconffilesdir
@@ -112,6 +121,8 @@ __SubmitFunctionalityTests_TestAll() {
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-run-hbaseperformanceeval-run-phoenixperformanceeval-run-zookeeperruok-functionality-testall
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-testall
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkpi-functionality-testall
+    # terasort w/ rawnetworkfs doesn't work, skip
+    # BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-hadoopterasort-run-sparkpi-functionality-testall
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-hadoopterasort-run-sparkpi-functionality-testall
     BasicJobSubmit magpie.${submissiontype}-storm-run-stormwordcount-run-zookeeperruok-functionality-testall
     BasicJobSubmit magpie.${submissiontype}-spark-with-zeppelin-run-sparkpi-run-checkzeppelinup-functionality-testall
@@ -125,6 +136,7 @@ __SubmitFunctionalityTests_InteractiveMode () {
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-functionality-interactive-mode
     BasicJobSubmit magpie.${submissiontype}-spark-functionality-interactive-mode
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-functionality-interactive-mode
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-functionality-interactive-mode
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-functionality-interactive-mode
     BasicJobSubmit magpie.${submissiontype}-storm-functionality-interactive-mode
     BasicJobSubmit magpie.${submissiontype}-spark-with-zeppelin-functionality-interactive-mode
@@ -132,10 +144,13 @@ __SubmitFunctionalityTests_InteractiveMode () {
 
 __SubmitFunctionalityTests_SetuponlyMode () {
     BasicJobSubmit magpie.${submissiontype}-hadoop-functionality-setuponly-mode
+    BasicJobSubmit magpie.${submissiontype}-hadoop-and-pig-functionality-setuponly-mode
+    BasicJobSubmit magpie.${submissiontype}-hadoop-and-mahout-functionality-setuponly-mode
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-functionality-setuponly-mode
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-functionality-setuponly-mode
     BasicJobSubmit magpie.${submissiontype}-spark-functionality-setuponly-mode
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-functionality-setuponly-mode
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-functionality-setuponly-mode
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-functionality-setuponly-mode
     BasicJobSubmit magpie.${submissiontype}-storm-functionality-setuponly-mode
     BasicJobSubmit magpie.${submissiontype}-spark-with-zeppelin-functionality-setuponly-mode
@@ -143,9 +158,13 @@ __SubmitFunctionalityTests_SetuponlyMode () {
 
 __SubmitFunctionalityTests_JobTimeout () {
     BasicJobSubmit magpie.${submissiontype}-hadoop-functionality-jobtimeout
+    BasicJobSubmit magpie.${submissiontype}-hadoop-and-pig-functionality-jobtimeout
+    BasicJobSubmit magpie.${submissiontype}-hadoop-and-mahout-functionality-jobtimeout
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-functionality-jobtimeout
+    BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-functionality-jobtimeout
     BasicJobSubmit magpie.${submissiontype}-spark-functionality-jobtimeout
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-functionality-jobtimeout
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-functionality-jobtimeout
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-functionality-jobtimeout
     BasicJobSubmit magpie.${submissiontype}-storm-functionality-jobtimeout
     BasicJobSubmit magpie.${submissiontype}-spark-with-zeppelin-functionality-jobtimeout
@@ -160,8 +179,8 @@ __SubmitFunctionalityTests_MagpieExports() {
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-functionality-checkexports
     BasicJobSubmit magpie.${submissiontype}-spark-functionality-checkexports
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-functionality-checkexports
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-functionality-checkexports
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-functionality-checkexports
-    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-rawnetworkfs-functionality-checkexports
     BasicJobSubmit magpie.${submissiontype}-storm-functionality-checkexports
     BasicJobSubmit magpie.${submissiontype}-zookeeper-functionality-checkexports
     BasicJobSubmit magpie.${submissiontype}-spark-with-zeppelin-functionality-checkexports
@@ -175,6 +194,7 @@ __SubmitFunctionalityTests_PrePostRunScripts() {
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-run-phoenixperformanceeval-functionality-prepostrunscripts
     BasicJobSubmit magpie.${submissiontype}-spark-run-sparkpi-functionality-prepostrunscripts
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-run-sparkwordcount-copy-in-functionality-prepostrunscripts
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-run-sparkwordcount-copy-in-functionality-prepostrunscripts
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-run-sparkwordcount-copy-in-functionality-prepostrunscripts
     BasicJobSubmit magpie.${submissiontype}-storm-run-stormwordcount-functionality-prepostrunscripts
     BasicJobSubmit magpie.${submissiontype}-spark-with-zeppelin-run-checkzeppelinup-functionality-prepostrunscripts
@@ -188,6 +208,7 @@ __SubmitFunctionalityTests_PreRunScriptError() {
     BasicJobSubmit magpie.${submissiontype}-hbase-with-hdfs-with-phoenix-functionality-prerunscripterror
     BasicJobSubmit magpie.${submissiontype}-spark-functionality-prerunscripterror
     BasicJobSubmit magpie.${submissiontype}-spark-with-hdfs-functionality-prerunscripterror
+    BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-functionality-prerunscripterror
     BasicJobSubmit magpie.${submissiontype}-spark-with-yarn-and-hdfs-functionality-prerunscripterror
     BasicJobSubmit magpie.${submissiontype}-storm-functionality-prerunscripterror
     BasicJobSubmit magpie.${submissiontype}-spark-with-zeppelin-functionality-prerunscripterror
