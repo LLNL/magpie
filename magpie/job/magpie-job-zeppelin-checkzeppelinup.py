2dea06204281544dbc9123ef81ad0810f4a6df18
Albert Chu <chu11@llnl.gov>
2016-08-15 23:10:30 -0700

    Support 'checkzeppelinup' option in ZEPPELIN_MODE

---
+++ magpie/job/magpie-job-zeppelin-checkzeppelinup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#############################################################################
+#  Copyright (C) 2013-2015 Lawrence Livermore National Security, LLC.
+#  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+#  Written by Albert Chu <chu11@llnl.gov>
+#  LLNL-CODE-644248
+#  
+#  This file is part of Magpie, scripts for running Hadoop on
+#  traditional HPC systems.  For details, see https://github.com/llnl/magpie.
+#  
+#  Magpie is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  Magpie is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with Magpie.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+
+import os
+import urllib2
+
+master_url = "http://" + os.environ['ZEPPELIN_MASTER_NODE'] + ":" + os.environ['ZEPPELIN_SERVER_PORT']
+
+print "Attempting to connect to: ", master_url
+
+response = urllib2.urlopen(master_url).getcode()
+
+if response == 200:
+    print "Success connecting to Zeppelin"
+else:
+    print "Failed connecting to Zeppelin - invalid response code:", response
