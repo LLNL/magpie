4b1a316c6146f169f67c84aba448779d3b97089e
Albert Chu <chu11@llnl.gov>
2017-07-01 23:50:52 -0700

    Support Zeppelin 0.7.2, update default to 0.7.2

--- misc/magpie-apache-download-and-setup.sh
+++ misc/magpie-apache-download-and-setup.sh
@@ -69,7 +69,7 @@ STORM_PACKAGE="storm/apache-storm-1.1.0/apache-storm-1.1.0.tar.gz"
 PHOENIX_PACKAGE="phoenix/apache-phoenix-4.10.0-HBase-1.2/bin/apache-phoenix-4.10.0-HBase-1.2-bin.tar.gz"
 PHOENIX_HBASE_PACKAGE="hbase/1.2.4/hbase-1.2.4-bin.tar.gz"
 KAFKA_PACKAGE="kafka/0.9.0.0/kafka_2.11-0.9.0.0.tgz"
-ZEPPELIN_PACKAGE="zeppelin/zeppelin-0.7.1/zeppelin-0.7.1-bin-all.tgz"
+ZEPPELIN_PACKAGE="zeppelin/zeppelin-0.7.2/zeppelin-0.7.2-bin-all.tgz"
 
 # First check some basics
 
