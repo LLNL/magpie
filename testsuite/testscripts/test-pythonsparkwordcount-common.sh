f747ef391669decda34a9ab30eff24033fb6e826
Albert Chu <chu11@llnl.gov>
2021-07-04 16:51:32 -0700

    testsuite: update pyspark tests for Spark 3.X

--- testsuite/testscripts/test-pythonsparkwordcount-common.sh
+++ testsuite/testscripts/test-pythonsparkwordcount-common.sh
@@ -2,7 +2,7 @@
 
 # first argument is master target
 cd $SPARK_HOME
-if echo ${SPARK_VERSION} | grep -q -E "2\.[0-9]\.[0-9]"
+if echo ${SPARK_VERSION} | grep -q -E "[2-3]\.[0-9]\.[0-9]"
 then
     command="bin/spark-submit"
 else
