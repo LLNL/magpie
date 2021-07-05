8bd238580e43bb68a5166866c49d8840805efff9
Albert Chu <chu11@llnl.gov>
2021-07-04 17:57:56 -0700

    testsuite: update spark-yarn test for Spark 2.X/3.X

--- testsuite/testscripts/test-pythonsparkwordcount-yarn.sh
+++ testsuite/testscripts/test-pythonsparkwordcount-yarn.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-${MAGPIE_SCRIPTS_HOME}/testsuite/testscripts/test-pythonsparkwordcount-common.sh yarn-client
+if echo ${SPARK_VERSION} | grep -q -E "1\.[0-9]\.[0-9]"
+then
+    sparkmaster="yarn-client"
+else
+    sparkmaster="yarn"
+fi
+
+${MAGPIE_SCRIPTS_HOME}/testsuite/testscripts/test-pythonsparkwordcount-common.sh ${sparkmaster}
