4e39128417b060d21f776f68d7876fb79895fb22
Albert Chu <chu11@llnl.gov>
2022-05-20 12:07:36 -0700

    Support Hadoop 3.2.3

--- testsuite/test-generate-hadoop.sh
+++ testsuite/test-generate-hadoop.sh
@@ -403,7 +403,7 @@ GenerateHadoopDependencyTests() {
     __GenerateHadoopDependencyTests_Dependency5 "5X" "n" 2.9.0 2.9.1
     __GenerateHadoopDependencyTests_Dependency5 "5Y" "n" 3.0.0 3.0.1 3.0.2 3.0.3
     __GenerateHadoopDependencyTests_Dependency5 "5Z" "n" 3.1.0 3.1.1 3.1.2 3.1.3 3.1.4
-    __GenerateHadoopDependencyTests_Dependency5 "5AA" "n" 3.2.0 3.2.1 3.2.2
+    __GenerateHadoopDependencyTests_Dependency5 "5AA" "n" 3.2.0 3.2.1 3.2.2 3.2.3
     __GenerateHadoopDependencyTests_Dependency5 "5AB" "n" 3.3.0 3.3.1
 
 # Dependency 6 test, detect newer hdfs version X from Y, HDFS over Lustre / NetworkFS
